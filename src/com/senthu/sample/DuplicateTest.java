778e188b5461229fc69ee803b7b15e2a320e5e09
Senthil Kumar M R <tosenthu@gmail.com>
2022-02-19 16:36:09 +0530

    Duplicate Set identification logic randomized for input strings

--- src/com/senthu/sample/DuplicateTest.java
+++ src/com/senthu/sample/DuplicateTest.java
@@ -3,17 +3,32 @@ package com.senthu.sample;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
+// Remove duplicate Strings
+// Remove Strings which are substring of other strings
 public class DuplicateTest {
 
 	public static void main(String[] args) {
 
-		String[] originalArray = { "ab", "ab", "ababcd", "cf", "cd", "cdag", "cfe" };
+		List<String> preparedStringArray = new ArrayList<String>();
+		for (int i = 0; i < new Random().nextInt(3, 20); i++) {
+			preparedStringArray.add(Utility.prepareStringFromCharacterArray(Utility.generateString(2, 3)));
+		}
+		for (int i = 0; i < new Random().nextInt(3, 10); i++) {
+			preparedStringArray.add(Utility.prepareStringFromCharacterArray(Utility.generateString(3, 5)));
+		}
+		
+		for (int i = 0; i < new Random().nextInt(3, 20); i++) {
+			preparedStringArray.add(Utility.prepareStringFromCharacterArray(Utility.generateString(5, 7)));
+		}
+		for (int i = 0; i < new Random().nextInt(3, 20); i++) {
+			preparedStringArray.add(Utility.prepareStringFromCharacterArray(Utility.generateString(7, 10)));
+		}
 
+		String[] originalArray = {};
+		originalArray = preparedStringArray.toArray(originalArray);
 		List<String> resultList = new ArrayList<>();
-
-		// Remove duplicate Strings
-		// Remove Strings which are substring of other strings
 
 		String[] sortedArray = originalArray.clone();
 		Arrays.sort(sortedArray);
@@ -33,6 +48,7 @@ public class DuplicateTest {
 		}
 		Utility.printArrayElements("Original Array", originalArray);
 		Utility.printArrayElements("Duplicate Removed Array", resultList.toArray());
+		System.out.println("\nNo of Duplicates Removed : " + (originalArray.length - resultList.size()));
 	}
 
 }
