2641768cb1980420ba188d825d89f28043d95d92
Senthil Kumar M R <tosenthu@gmail.com>
2022-02-19 16:24:58 +0530

    code review

--- src/com/senthu/sample/PositionalBubbleSort.java
+++ src/com/senthu/sample/PositionalBubbleSort.java
@@ -4,8 +4,8 @@ public class PositionalBubbleSort {
 
 	private static final Boolean ODD_POSITION_SORT = true;
 
-	private static void bubbleSort(boolean odd, int[] arrayElements) {
-		for (int i = odd ? 0 : 1; i < (arrayElements.length - 1); i += 2) {
+	private static void bubbleSort(int[] arrayElements) {
+		for (int i = PositionalBubbleSort.ODD_POSITION_SORT ? 0 : 1; i < (arrayElements.length - 1); i += 2) {
 			for (int j = i + 2; j < arrayElements.length; j += 2) {
 				if (arrayElements[i] > arrayElements[j]) {
 					Utility.swap(arrayElements, i, j);
@@ -17,7 +17,7 @@ public class PositionalBubbleSort {
 	public static void main(String[] args) {
 		int[] arrayElements = Utility.generateRandomIntArray(15, 20, 10, 99);
 		Utility.printArrayElements("Before Bubble Sort", arrayElements);
-		PositionalBubbleSort.bubbleSort(PositionalBubbleSort.ODD_POSITION_SORT, arrayElements);
+		PositionalBubbleSort.bubbleSort(arrayElements);
 		Utility.printArrayElements("After Bubble Sort", arrayElements);
 	}
 }
