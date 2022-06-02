ff35c6cb470e059b9b0f5c0cb19fac789f679a00
Senthil Kumar M R <tosenthu@gmail.com>
2022-06-02 22:47:49 +0530

    java new version changes

---
+++ src/com/senthu/sample/FunctionFTUsage.java
@@ -0,0 +1,10 @@
+package com.senthu.sample;/**
+ *  FunctionFTUsage : FunctionFTUsage 
+ *
+ * @author : Senthil Kumar Manguzhiparambil Rangan (smanguzh@cisco.com)
+ * @version : 1.0 (Jun 01, 2022)
+ * @since : 1.0 (Jun 01, 2022)
+ *
+ */
+ public class FunctionFTUsage {
+}
